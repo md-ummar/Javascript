6ae2b553f848eb4b89839378600d49dcd1c33b4e
md ummar <77294756+md-ummar@users.noreply.github.com>
2024-01-29 14:04:23 +0000

    JS basics

---
+++ JS basics/Variabels.js
@@ -0,0 +1,15 @@
+// Javascript variables:
+
+const accountID = 12545; //const cannot be changed once declared
+
+let accountName = "Hitesh"; //It can be changed
+
+// var should not be used as it has issues in block scope;
+
+//Data Types:
+
+let age = 23; // number
+
+let name = "hitesh"; //string
+
+console.log(typeof(name));
