6125131fefb4552b9720e25da8647203601ed944
md ummar <77294756+md-ummar@users.noreply.github.com>
2024-02-05 14:21:54 +0000

    functions and scopes

--- JS_basics/conversions.js
+++ JS_basics/conversions.js
@@ -1,13 +1,46 @@
 // data type conversion;
 
-let score = "33";
+let score = true;
 
 let number = Number(score);
 
-console.log(typeof score);
+//console.log(typeof score);
 
 //conversion of sstring to number examples;
-score = "33abc";
+
 number = Number(score);
 
-console.log(number);
+console.log(number); // NaN not a number;
+
+/* 
+if the "33abc" => NaN
+"hitesh"=> NaN
+null=> 0
+
+*/
+
+//let result = 1;
+
+//let isResult = Boolean(result);
+
+//console.log(isResult);
+
+//Comparisions to avoid:
+//null >= 0;
+//null == 0;
+//null>0;
+
+//undefined >=0;
+//undefined == 0;
+
+// NOTE: equality check operators and comparisions operators work differently;
+
+// === strick check --> used to strick check the datatypes without converting. Datatypes should be same;
+
+
+//Return type of datatypes:
+/* number --> Number
+boolean --> Boolean
+string --> String
+null --> object
+function --> object function */
---
+++ JS_basics/string.js
@@ -0,0 +1,9 @@
+const myArr = [1,2,3,4,5,6,7]
+
+console.log(myArr.slice(1,3));
+console.log(myArr);
+
+const newArr = new Array(1,2,3,4,5)
+
+console.log(newArr.splice(1,3));
+console.log(newArr);
